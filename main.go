82f52a4974d09be3651381e75d5195f0240965c5
Camilo Cueto <camilo@ccueto.com>
2023-05-07 19:03:29 -0400

    Add unhandled error

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
+	errorFunc("This should error and we don't handle it..")
 	fmt.Println("Hello world!")
 }
 
 func unusedFunc() {
 	fmt.Println("unused")
+}
+
+func errorFunc(msg string) error {
+	fmt.Println(msg)
+	return errors.New("")
 }
